13aa8692381cc39b97b240dd3b3260857bfebe83
MaksymPetyak <petyak.mi@gmail.com>
2023-07-29 00:04:43 +0200

    Set up empty screen message

--- components/empty-screen.tsx
+++ components/empty-screen.tsx
@@ -1,56 +1,52 @@
-import { UseChatHelpers } from 'ai/react'
+import {UseChatHelpers} from 'ai/react'
 
-import { Button } from '@/components/ui/button'
-import { ExternalLink } from '@/components/external-link'
-import { IconArrowRight } from '@/components/ui/icons'
+import {Button} from '@/components/ui/button'
+import {ExternalLink} from '@/components/external-link'
+import {IconArrowRight} from '@/components/ui/icons'
 
 const exampleMessages = [
-  {
-    heading: 'Explain technical concepts',
-    message: `What is a "serverless function"?`
-  },
-  {
-    heading: 'Summarize an article',
-    message: 'Summarize the following article for a 2nd grader: \n'
-  },
-  {
-    heading: 'Draft an email',
-    message: `Draft an email to my boss about the following: \n`
-  }
+    {
+        heading: 'Explain technical concepts',
+        message: `What is a "serverless function"?`
+    },
+    {
+        heading: 'Summarize an article',
+        message: 'Summarize the following article for a 2nd grader: \n'
+    },
+    {
+        heading: 'Draft an email',
+        message: `Draft an email to my boss about the following: \n`
+    }
 ]
 
-export function EmptyScreen({ setInput }: Pick<UseChatHelpers, 'setInput'>) {
-  return (
-    <div className="mx-auto max-w-2xl px-4">
-      <div className="rounded-lg border bg-background p-8">
-        <h1 className="mb-2 text-lg font-semibold">
-          Welcome to Next.js AI Chatbot!
-        </h1>
-        <p className="mb-2 leading-normal text-muted-foreground">
-          This is an open source AI chatbot app template built with{' '}
-          <ExternalLink href="https://nextjs.org">Next.js</ExternalLink> and{' '}
-          <ExternalLink href="https://vercel.com/storage/kv">
-            Vercel KV
-          </ExternalLink>
-          .
-        </p>
-        <p className="leading-normal text-muted-foreground">
-          You can start a conversation here or try the following examples:
-        </p>
-        <div className="mt-4 flex flex-col items-start space-y-2">
-          {exampleMessages.map((message, index) => (
-            <Button
-              key={index}
-              variant="link"
-              className="h-auto p-0 text-base"
-              onClick={() => setInput(message.message)}
-            >
-              <IconArrowRight className="mr-2 text-muted-foreground" />
-              {message.heading}
-            </Button>
-          ))}
+export function EmptyScreen({setInput}: Pick<UseChatHelpers, 'setInput'>) {
+    return (
+        <div className="mx-auto max-w-2xl px-4">
+            <div className="rounded-lg border bg-background p-8">
+                <h1 className="mb-2 text-lg font-semibold">
+                    Welcome to patient simulator!
+                </h1>
+                <p className="mb-2 leading-normal text-muted-foreground">
+                    For this case you will be seeing Jason Brody, a 26 year-old patient
+                    of yours for
+                    the past 3 years in overall good health. Jason is coming in today
+                    for your periodic health visit. As is his custom, he came in last
+                    week to have lab work drawn in preparation for his visit.
+
+                    <span className={"font-semibold"}>
+                        <br/>
+                        In reviewing his lab results, you find that his HIV ELISA/Western Blot
+                        (which had been done by verbal consent) has returned positive. He is
+                        not yet aware of this result.
+                        <br/>
+                    </span>
+
+                    Perform an appropriate history, disclose the results, and manage.
+                </p>
+                {/*<p className="leading-normal text-muted-foreground">*/}
+                {/*    */}
+                {/*</p>*/}
+            </div>
         </div>
-      </div>
-    </div>
-  )
+    )
 }
