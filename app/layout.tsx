863ef1ff398e19e5169cf877416d7905d52acff5
MaksymPetyak <petyak.mi@gmail.com>
2023-07-31 15:18:34 +0200

    Change title

--- app/layout.tsx
+++ app/layout.tsx
@@ -11,10 +11,10 @@ import { Header } from '@/components/header'
 
 export const metadata: Metadata = {
   title: {
-    default: 'Next.js AI Chatbot',
-    template: `%s - Next.js AI Chatbot`
+    default: 'Patient Simulator',
+    template: `Patient Simulator`
   },
-  description: 'An AI-powered chatbot template built with Next.js and Vercel.',
+  description: '',
   themeColor: [
     { media: '(prefers-color-scheme: light)', color: 'white' },
     { media: '(prefers-color-scheme: dark)', color: 'black' }
